ec0d117187d003bb720506650baac3290b71a92c
Himjune <himjune@mail.ru>
2018-08-14 11:09:41 +0300

    Fin auth webhook

--- start.sh
+++ start.sh
@@ -3,8 +3,7 @@
 
 if [ ! -f /var/run/auth.pid ]; then
     cd auth
-    AUTH_PID=`exec ./auth &`
-    echo $AUTH_PID >> /var/run/auth.pid
+    ./auth & echo $! >>/var/run/auth.pid
 else
     echo "AUTH PID EXISTS"
 fi
