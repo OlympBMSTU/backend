b74bae9867241c61575ad279d3167967a552e2bf
Himjune <himjune@mail.ru>
2018-08-14 10:41:14 +0300

    Fix stop

--- stop.sh
+++ stop.sh
@@ -1,6 +1,10 @@
 #! /bin/sh
 
-AUTH_PID=`cat /var/run/auth.pid`
-echo "$AUTH_PID"
-kill $AUTH_PID
-rm /var/run/auth.pid
+if [ -f /var/run/auth.pid ]; then
+    AUTH_PID=`cat /var/run/auth.pid`
+    echo "$AUTH_PID"
+    kill $AUTH_PID
+    rm /var/run/auth.pid
+else
+    echo "Nothing"
+fi
