09787e0efc686224b1a7690d566ef6594b920918
Himjune <himjune@mail.ru>
2018-08-27 19:51:27 +0300

    fix

--- auth/app/models/account.js
+++ auth/app/models/account.js
@@ -1,5 +1,6 @@
 const crypto = require('crypto');
-const Op = sequelize.Op;
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 const hashSecret = '21477 61225 37 7836 29 2364? 32 575 784 9383.';
 
