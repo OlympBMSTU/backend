054de945c09cd81b4f5c25fa9c42d8190b986fd9
Himjune <himjune@mail.ru>
2018-08-29 03:45:30 +0300

    Try add selector for localhsot cors

--- auth/app.js
+++ auth/app.js
@@ -14,7 +14,12 @@ winston.configure({
 
 const app = express();
 app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "http://80.87.193.245, http://127.0.0.1");
+  let allow = "http://80.87.193.245";
+  if (req.get("Origin") == "http://127.0.0.1") {
+    allow = "http://127.0.0.1";
+  }
+  
+	res.header("Access-Control-Allow-Origin", allow);
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 	res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, DELETE, PATCH")
 	next();
