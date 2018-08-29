a9252eb807fa9646fd2a371240254e1a25d36081
Himjune <himjune@mail.ru>
2018-08-29 03:33:11 +0300

    Fixing cors enabling

--- auth/app.js
+++ auth/app.js
@@ -14,7 +14,7 @@ winston.configure({
 
 const app = express();
 app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "http://80.87.193.245/");
+	res.header("Access-Control-Allow-Origin", "http://80.87.193.245");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 	res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, DELETE, PATCH")
 	next();
