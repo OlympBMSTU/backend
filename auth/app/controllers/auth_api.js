c3484949f06e8f43edbf41d3dfb2b1423474a351
Himjune <himjune@mail.ru>
2018-09-10 06:07:05 +0300

    fix turnng of req

--- auth/app/controllers/auth_api.js
+++ auth/app/controllers/auth_api.js
@@ -31,6 +31,7 @@ router.post('/register', (req, res, next) => {
 		return res.status(200).send({res_code: "INVALID", res_data: req.body, res_msg: "Заполните все поля"});
 	} else {
 		*/
+		let captcha = req.body['g-recaptcha-response'];
 		reCaptcha.check(captcha, function (captchaReqErr, captchaReqRes) {
 			if (captchaReqErr) {
 				return res.status(200).send({res_code: "CAPTCHA_ERROR", res_data: captchaReqErr, res_msg: "Произошла ошибка во время проверки reCaptcha"} );
